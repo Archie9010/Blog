aba33fb4e400fdb6943b28fb4b514c49ece54e7e
Archie9010 <archie9010@protonmail.com>
2022-11-07 15:12:12 +0000

    Add an edit profile html page

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=250, default='city')
+    snippet = models.CharField(max_length=250, default='Click link to read blog post!')
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def total_likes(self):
