df7a9611e90e4b0e299218088fca99c8880c6fe3
Archie9010 <archie9010@protonmail.com>
2022-11-08 09:17:08 +0000

    Costumise user profile add bio to user

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,13 @@ class Category(models.Model):
         return reverse('home')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
