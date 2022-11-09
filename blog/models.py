617edcde438ba53f3f36e1000f8f94f2dd5987cb
Archie9010 <archie9010@protonmail.com>
2022-11-09 12:18:29 +0000

    Add comment section

--- blog/models.py
+++ blog/models.py
@@ -31,8 +31,6 @@ class Profile(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
-    
-
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
@@ -55,3 +53,13 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    body= models.TextField()
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
