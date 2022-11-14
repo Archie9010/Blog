1110849704b8261d6ccabff0483e1bd47405e66e
Archie9010 <archie9010@protonmail.com>
2022-11-14 11:49:43 +0000

    Add initial commit to readme

--- blog/views.py
+++ blog/views.py
@@ -79,6 +79,7 @@ class AddCommentView(CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'add_comment.html'
+    
     def form_valid(self, form):
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
