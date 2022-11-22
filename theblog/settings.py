c0624f8677ef758858d7f53fa021df66549a8ecb
Archie9010 <archie9010@protonmail.com>
2022-11-22 09:53:24 +0000

    Add images to readme.md chaged colours in css code

--- theblog/settings.py
+++ theblog/settings.py
@@ -146,6 +146,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+
+#SMTP Configuration 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('artchi1090@gmail.com') 
+EMAIL_HOST_PASSWORD = os.environ.get('Ramzes1!')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
