7ae1ea920d7e6ef8abc67cb14bed00ff7b38f8b4
Archie9010 <archie9010@protonmail.com>
2022-11-25 11:35:32 +0000

    add currently logged as user note to change password form

--- members/forms.py
+++ members/forms.py
@@ -40,12 +40,13 @@ class EditProfileForm(UserChangeForm):
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+ 
+   
     
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
     
 class PasswordChangingForm(PasswordChangeForm):
