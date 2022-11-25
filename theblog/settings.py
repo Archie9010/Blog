40e5ae4dac470f7a20eb0004e45004ba3fbc195b
Archie9010 <archie9010@protonmail.com>
2022-11-25 08:02:46 +0000

    deployment commit

--- theblog/settings.py
+++ theblog/settings.py
@@ -30,8 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['blog-archie.herokuapp.com', 'localhost']
 
