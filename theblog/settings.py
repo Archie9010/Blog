4a7ba713b9d6d09ab6e6bcafd4059165ba30e142
Archie9010 <archie9010@protonmail.com>
2022-11-30 12:28:29 +0000

    add 405 error handler

--- theblog/settings.py
+++ theblog/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'theblog.wsgi.application'
 
 # if development:
 
-#     DATABASES = {
+# DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.sqlite3',
 #             'NAME': BASE_DIR / 'db.sqlite3',
--- theblog/views.py
+++ theblog/views.py
@@ -5,18 +5,25 @@ def error_404_view(request, exception):
     """
     Custom 404 page
     """
-    return render(request, "404.html", status=404)
+    return render(request, "errors/404.html", status=404)
 
 
 def error_500_view(request):
     """
     Custom 500 page
     """
-    return render(request, "500.html", status=500)
+    return render(request, "errors/500.html", status=500)
 
 
 def error_403_view(request, exception):
     """
     Custom 403 page
     """
-    return render(request, "403.html", status=403)
+    return render(request, "errors/403.html", status=403)
+
+
+def error_405_view(request, exception):
+    """
+    Custom 405 page
+    """
+    return render(request, "errors/405.html", status=403)
